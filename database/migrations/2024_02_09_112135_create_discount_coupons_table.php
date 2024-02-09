3b1abd8f453cff8887d45c397b607a9841e88015
Wealth Max <vivek.sharma1275@gmail.com>
2024-02-09 18:56:19 +0530

    coupon added

---
+++ database/migrations/2024_02_09_112135_create_discount_coupons_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_coupons', function (Blueprint $table) {
+            $table->id();
+
+            // The discount coupon code
+            $table->string('code');
+
+            // The human readable discount coupon code name
+            $table->string('name')->nullable();
+
+            // The human readable discount coupon code name
+            $table->string('description')->nullable();
+
+            // The max uses this coupon code
+            $table->integer('max_uses')->nullable();
+
+            // How many times a user can use this coupon 
+            $table->integer('max_uses_user')->nullable();
+
+            // Whether or not the coupon is a percentage or a fixed price.
+            $table->enum('type', ['percent', 'fixed'])->default('fixed');
+
+            // The amount to discount based on type 
+            $table->double('discount_amount', 10, 2);
+
+            // The amount to discount based on minimum amount
+            $table->double('min_amount', 10, 2)->nullable();
+
+
+            $table->integer('status')->default(1);
+
+
+            // when the coupon starts 
+            $table->timestamp('starts_at')->nullable();
+
+            // When the coupon ends 
+            $table->timestamp('ends_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_coupons');
+    }
+};
