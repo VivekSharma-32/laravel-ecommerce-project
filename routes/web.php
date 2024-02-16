2b2ed56c600c4532502fe9c6a6f7538ac4b6236d
Wealth Max <vivek.sharma1275@gmail.com>
2024-02-16 15:53:33 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -120,6 +120,9 @@ Route::group(['prefix' => 'admin'], function () {
         Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
         Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.delete');
 
+        Route::get('/ratings', [ProductController::class, 'productRatings'])->name('products.productRatings');
+        Route::get('/change-rating-status', [ProductController::class, 'changeRatingStatus'])->name('products.changeRatingStatus');
+
 
         Route::get('/get-products', [ProductController::class, 'getProducts'])->name('products.getProducts');
 
